cc7cfb79fc1843c258b1f90ecb54325e199e77d0
shahram moradpour <51191070+strumer69@users.noreply.github.com>
2025-07-17 20:37:56 +0200

    Update hello_MPI.cpp

--- hello_MPI.cpp
+++ hello_MPI.cpp
@@ -1,9 +1,17 @@
 #include <mpi.h>
 #include <iostream>
-int main(){
-    std::cout << "Hello MPI?" <<std::endl;
-    MPI_Init(0,0);
-    // part of code that you want to parallelize
-    MPI_Finalize();
+
+
+//Main function that takes command-line arguments. These are required for MPI initialization.
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);  // Initializes the MPI environment. This must be called before any other MPI functions.
+
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Get total number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get rank of the process
+
+    std::cout << "Hello from process " << world_rank << " out of " << world_size << " processes\n";
+
+    MPI_Finalize(); // Finalize MPI
     return 0;
 }
